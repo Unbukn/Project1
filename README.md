5d749c1681ff4a146f41d0c87d5694bd535389fd
cheese stick <61661631+longhike@users.noreply.github.com>
2020-06-25 18:24:12 -0400

    to pull master

--- README.md
+++ README.md
@@ -1,60 +1,61 @@
-### Temperature
-![Alt text](./Assets/IMG_1163.jpeg?raw=true "Design")
+# Temperature
+![Alt text](.Assets/LogoMakr_9JmX12.png?raw=true "Design")
 
-# Created on 6/20/2020
-# By Sam Ascheim, James Hudgins, Bashkim Ereql, Alejandro Escanellas 
+### Created on 6/20/2020
+### Last Update on 06/25/20
+### Created by Sam Ascheim, Alejandro Escanellas, Bashkim Ereql, James Hudgins 
 
 ## Description:
 Temperature is a portal to help individuals begin their day on the right note. The user is able to select one of the three mood options (happy, sad, or neither), each of which will populate the page with quotes and content to enhance the user's mood and inspire them to seize the day.
 
-# Target Audience/ Rating:
-    Individual users
-# Use Case / Product Application:
-    Temperature is an application provides users with articles and quotes tailored to their current mood. Content provided aims to promote a stimulating environment in which a users can ruminate, reflect on, and process their inner-most feelings.
+## Target Audience & Rating:
+Temperature is targeted for individuals. Rated T for Teen (Ages 13+). Titles rated T (Teen) have content that may be suitable for ages 13 and older. Titles in this category may contain violence, suggestive themes, crude humor, minimal blood, simulated gambling, and/or infrequent use of strong language.
 
-## Set-up & Installation requirements:
-# Set-up:
-To use Temperature you simply need to know your physical location and mental state.
+## Use Case / Product Application:
+Temperature is an application that provides users with articles and quotes tailored to their current mood. Content provided aims to promote a stimulating environment in which users can ruminate, reflect on, and process their innermost feelings.
 
-# System Requirements
-a web browser with internet connectivity.
+## Application Overview:
+> # Technology Used
+> - HyperText Markup Language
+> - Cascading Styles Sheets
+> - Foundations StyleSheet Framework
+> - javascript ES6
+> - jQuery
+> - Moments.js
+> - Graydyn Synonyms API 
+> - Quotable Quotes API
+> - Best Quotes API
+> - New York Times Article search API
 
-# Acceptance Criteria:
+
+### System Requirements
+To use Temperature you simply need to know your physical location, mental state and have access to a web browser with internet connectivity.
+
+### Minimum Viable Product (MVP)
+The Temperature MVP release shall accomplish the following:
+WHEN user selects the ‘Great’ button and an inspirational quote, and a related NTY news article is displayed.
+WHEN user selects the ‘notSure’ button and a random quote, and a New York Times article is displayed.
+WHEN user selects the ‘notGreat’ button and an uplifting quote, and a positive New York Times article is displayed.
+After results are displayed, the user can select the ‘reset’ button and will see the current results be cleared and the initial buttons displayed.
+
+
+### Architecture Diagram
+![Alt text](.Assets/arch.png?raw=true "Design")
 
 ## Known Bugs:
-TBD
-
-## Support & Contact details:
-
-e-mail sam.ascheim@gmail.com
-
-## Technologies Used
-
-HTML
-
-CSS
-
-Foundations CSS
-
-Javascript
-
-JQUERY
-
-Moments.js API
-
-Quotable Quotes
-
-Bestquotes
-
-New York Times 
+None
 
 ## Contribution Guidelines 
-​
 Direct link to site: https://unbukn.github.io/Project1/
+Direct link to Documentation: https://docs.google.com/document/d/1WhiaeWRMq0XmhxBcRfWZ1QtyxVUZkL_ZFdk6OJIATM8/
 
-​
+## Development Team
+![Alt text](.Assets/team.png?raw=true "Design")
+
+## Support
+If you need assistance please send email to: ereqibashkim@gmail.com, james.hudgins.work@gmail.com, sam.ascheim@gmail.com, alejandroescanellas@gmail.com
+
 ### License
-​
 Copyright(c) 2020 By Sam Ascheim, James Hudgins, Bashkim Ereql, Alejandro Escanellas
-​
+
 This software is licensed under MIT license.
--- logic.js
+++ logic.js
@@ -13,10 +13,120 @@ var ranQuote = {
 		"x-rapidapi-host": "quotable-quotes.p.rapidapi.com",
 		"x-rapidapi-key": "d82c676afemsh7edaf163ce44088p1641dajsn681616f67751"
 	}
+// var search = "dogs"
+// var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=" + search + "&api-key=fUAV70oYpTeS4X52k9Ce031QOrQOi4Tg";
+// $.ajax({
+// 	url: queryURL,
+// 	method: "GET"
+//   }).then(function(res2) {
+// 	  console.log(res2)
+
+//   })
+
+// function to be called at each "how are you feeling" button event listener
+  function replaceButtons() {
+	  // replace header in the card with reset text
+	$("#headerQuestion").text("Click 'reset' to start again!")
+	// empty the question div of all buttons
+	$("#questionButtons").empty()
+	// var to create a new a element
+	var resetDiv = $("<a>")
+	// add attributes: class "button", id "resetButton", text "Reset"
+	resetDiv.attr("class", "button")
+	resetDiv.attr("id", "resetButton")
+	resetDiv.text("Reset")
+	// append to the question div
+	$(resetDiv).appendTo("#questionButtons");
 }
 
-$.ajax(ranQuote).done(function (response) {
-	console.log(response);
+// clicker to reset the page
+function resetPage() {
+	$("#resetButton").on("click", function (event) {
+		event.preventDefault()
+	// reload the page
+	// console.log("reset button clicked")
+		location.reload()
+	});
+}
+
+//   event listener for great btn
+$("#great").on("click", function (event) {
+	event.preventDefault()
+	// random list of search terms
+	var sadSearchTerms = ["poor", "woeful", "deplorable", "measly", "misfortune", "hapless", "scummy", "pitiful", "pathetic"]
+	// select random search term to get synonyms for
+	var theRandomTerm = sadSearchTerms[Math.floor(Math.random() * sadSearchTerms.length)]
+
+	// first get a random var from https://rapidapi.com/user/Graydyn
+	var synonymsSearch = {
+		"async": true,
+		"crossDomain": true,
+		"url": "https://languagetools.p.rapidapi.com/synonyms/"+theRandomTerm,
+		"method": "GET",
+		"headers": {
+			"x-rapidapi-host": "languagetools.p.rapidapi.com",
+			"x-rapidapi-key": "d82c676afemsh7edaf163ce44088p1641dajsn681616f67751",
+			"accept": "application/json"
+		}
+	}
+	// next use that term to get a quote related
+	$.ajax(synonymsSearch).done(function (Graydyn) {
+		// console.log(Graydyn.synonyms.length);
+		var GraydynSearchTerm = Graydyn.synonyms[Math.floor(Math.random() * Graydyn.synonyms.length)]
+
+			// nested ajax request for sad quotes with gradynSearch Term
+			var sadQuote = {
+				"async": true,
+				"crossDomain": true,
+				"url": "https://qvoca-bestquotes-v1.p.rapidapi.com/quote?message="+GraydynSearchTerm,
+				"method": "GET",
+				"headers": {
+					"x-rapidapi-host": "qvoca-bestquotes-v1.p.rapidapi.com",
+					"x-rapidapi-key": "d82c676afemsh7edaf163ce44088p1641dajsn681616f67751"
+				}
+			}
+			$.ajax(sadQuote).done(function (bestQuotes) {
+				// console.log(bestQuotes);
+			// create var containing results from Quotes
+			// var quote
+			var theQuote = bestQuotes.message
+			
+			// var author
+			var theAuthor = bestQuotes.author
+				
+			// empty the results div
+			$("#results").empty();
+			// creat new div
+			var newDiv = $("<div>")
+			
+			// apply the card and answerCard class to the new div
+			newDiv.addClass("card answerCard");
+			
+			// create a new para for the quote
+			var newPara = $("<h3>")
+
+			// apply quote text to the para
+			newPara.text(theQuote)
+			newPara.attr("class", "quotePara")
+			
+			// second para for author
+			var newPara2 = $("<p>")
+			// apply the author text to the second para
+			newPara2.text(" - " + theAuthor)
+			newPara2.attr("class", "quoteAuth")
+
+
+			// append the new div to the results div
+			$(newDiv).appendTo("#results");
+			// append the paras to the new div
+			newPara.appendTo(newDiv);
+			newPara2.appendTo(newDiv);
+
+			// remove the original buttons div
+			replaceButtons()
+			resetPage()
+
+		})
 });
 // ajax request for sad quotes
 var sadQuote = {
@@ -30,9 +140,8 @@ var sadQuote = {
 	}
 }
 
-$.ajax(sadQuote).done(function (res) {
-	console.log(res);
-});
+
+}) 
 
 var search = "dogs"
 
@@ -93,6 +202,30 @@ $("#great").on("click", function (event) {
 	
 	// var author
 	var theAuthor = bestQuotes.author
+//   event listener for not sure btn
+$("#notSure").on("click", function (event) {
+	event.preventDefault()
+// ajax request for random quote
+var getRandomeQuote = {
+	"async": true,
+	"crossDomain": true,
+	"url": "https://timshim-quotes-v1.p.rapidapi.com/quotes",
+	"method": "GET",
+	"headers": {
+		"x-rapidapi-host": "timshim-quotes-v1.p.rapidapi.com",
+		"x-rapidapi-key": "d82c676afemsh7edaf163ce44088p1641dajsn681616f67751"
+	}
+}
+
+$.ajax(getRandomeQuote).done(function (ranQuote) {
+	// console.log(ranQuote);
+
+	// create var containing results from Quotes
+	// var quote
+	var theQuote = ranQuote.quoteText
+	// console.log(theQuote)
+	// var author
+	var theAuthor = ranQuote.author
 		
 	// empty the results div
 	$("#results").empty();
@@ -104,6 +237,7 @@ $("#great").on("click", function (event) {
 	
 	// create a new para for the quote
 	var newPara = $("<p>")
+	var newPara = $("<h3>")
 
 	// apply quote text to the para
 	newPara.text(theQuote)
@@ -113,6 +247,8 @@ $("#great").on("click", function (event) {
 	var newPara2 = $("<p>")
 	// apply the author text to the second para
 	newPara2.text(theAuthor)
+	newPara2.text(" - " + theAuthor)
+	newPara2.attr("class", "quoteAuth")
 
 
 	// append the new div to the results div
@@ -143,6 +279,7 @@ $("#notGreat").on("click", function (event) {
 	}
 	$.ajax(happyQuotes).done(function (bestQuotes) {
 		console.log(bestQuotes);
+		// console.log(bestQuotes);
 	// create var containing results from Quotes
 	// var quote
 	var theQuote = bestQuotes.message
@@ -160,6 +297,7 @@ $("#notGreat").on("click", function (event) {
 	
 	// create a new para for the quote
 	var newPara = $("<p>")
+	var newPara = $("<h3>")
 
 	// apply quote text to the para
 	newPara.text(theQuote)
@@ -169,6 +307,8 @@ $("#notGreat").on("click", function (event) {
 	var newPara2 = $("<p>")
 	// apply the author text to the second para
 	newPara2.text(theAuthor)
+	newPara2.text(" - " + theAuthor)
+	newPara2.attr("class", "quoteAuth")
 
 
 	// append the new div to the results div
